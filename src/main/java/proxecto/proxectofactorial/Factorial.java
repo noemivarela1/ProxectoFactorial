e8ceec529b98e5a21ca578a439565f868a67ca33
Noemí <Noem@PortatilCasa>
2022-01-25 09:16:46 +0100

    Primeiro commit da clase Factorial e creado esqueleto das probas para a clase Factorial.

---
+++ src/main/java/proxecto/proxectofactorial/Factorial.java
@@ -0,0 +1,22 @@
+package proxecto.proxectofactorial;
+/**
+ * Calcula o factorial de n
+ */
+import java.util.Scanner;
+public class Factorial {
+    public static int factorial(int num){
+        int factorial=1;
+        
+        //n!=1*2*3*...*n
+        for (int i=1;i<=num;i++){
+            factorial*=i;
+        }
+        return factorial;
+    }
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        System.out.println("Introduce o número para calcular o factorial");
+        int n=in.nextInt();
+        System.out.println("O factorial de "+n+" é "+factorial(n));
+    }
+}
