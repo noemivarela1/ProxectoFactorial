bbd89015feca240ede1d9f016f13aae268d62297
Noemí <Noem@PortatilCasa>
2022-01-27 11:01:20 +0100

    Realización do test de proba do factorial para o número 1

--- src/test/java/proxecto/proxectofactorial/FactorialTest.java
+++ src/test/java/proxecto/proxectofactorial/FactorialTest.java
@@ -22,16 +22,16 @@ public class FactorialTest {
      */
     @Test
     public void testFactorial1() {
-        System.out.println("factorial1");
+        System.out.println("factorial 0");
         int num = 0;
-        int resultadoEsperado = 0;
+        int resultadoEsperado = 1;
         int resultado = Factorial.factorial(num);
         assertEquals(resultadoEsperado, resultado);
     }
 
     @Test
     public void testFactorial2(){
-        System.out.println("factorial2");
+        System.out.println("factorial 120");
         int num=5;
         int resultadoEsperado=120;
         int resultado=Factorial.factorial(num);
@@ -40,6 +40,10 @@ public class FactorialTest {
     
     @Test
     public void testFactorial3(){
-        System.out.println("factorial3");
+        System.out.println("factorial 1");
+        int num=1;
+        int resultadoEsperado=1;
+        int resultado=Factorial.factorial(num);
+        assertEquals(resultadoEsperado,resultado);
     }
 }
