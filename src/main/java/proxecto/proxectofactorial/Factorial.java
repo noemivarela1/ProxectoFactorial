8583ca51a04c380e7b5d9bd7d743753302fe115e
Noemí <Noem@PortatilCasa>
2022-01-28 11:17:11 +0100

    Realización do factorial para números long

--- src/main/java/proxecto/proxectofactorial/Factorial.java
+++ src/main/java/proxecto/proxectofactorial/Factorial.java
@@ -13,10 +13,22 @@ public class Factorial {
         }
         return factorial;
     }
+    public static long factorial(long num){
+        long factorial=1;
+        
+        //n!=1*2*3*...*n
+        for (int i=1;i<=num;i++){
+            factorial*=i;
+        }
+        return factorial;
+    }
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
         System.out.println("Introduce o número para calcular o factorial");
         int n=in.nextInt();
         System.out.println("O factorial de "+n+" é "+factorial(n));
+        System.out.println("Introduce un número para calcular o factorial");
+        long nl=in.nextLong();
+        System.out.println("O factorial de "+n+" é "+factorial(nl));
     }
 }
